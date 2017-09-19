ed005c9126534437ed1479144f94f5a85b36878e
GibboK <gibbok.coding@gmail.com>
2017-09-19 17:37:48 +0200

    Add utility removeFileExtension

--- app/src/utility.js
+++ app/src/utility.js
@@ -1,3 +1,7 @@
 export const createActiveCssClass = (className, isActive) => {
   return `${className}${isActive ? '--active' : ''}`
 }
+
+export const removeFileExtension = (fileName) => {
+  return fileName.split('.')[0]
+}
