9f4f3442501f4c8cb57443a8e8210d267f24c64c
GibboK <gibbok.coding@gmail.com>
2017-09-20 23:04:41 +0200

    Fix missing alt tag on image Fix a warning in prod build

--- app/src/document/Footer.js
+++ app/src/document/Footer.js
@@ -7,7 +7,7 @@ const Footer = ({onClickButton}) => {
   return (
     <div className='document__footer'>
       <div className='document__footer__agenda'>Agenda item</div>
-      <div className='document__footer__menu'><img src={select} /></div>
+      <div className='document__footer__menu'><img src={select} alt='' /></div>
       <div className='document__footer__add'>
         <Button label={'Add'} width={93} height={35} onClickButton={onClickButton} />
       </div>
