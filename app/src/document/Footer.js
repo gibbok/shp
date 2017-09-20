373f7889fb0214879fc738e215714686d1af7c91
GibboK <gibbok.coding@gmail.com>
2017-09-20 14:31:02 +0200

    Add styles to document Footer

--- app/src/document/Footer.js
+++ app/src/document/Footer.js
@@ -1,11 +1,16 @@
 import React from 'react'
 import './Footer.css'
+import select from './select.png'
 
 const Footer = () => {
   return (
     <div className='document__footer'>
-      Footer
+      <div className='document__footer__agenda'>Agenda item</div>
+      <div className='document__footer__menu'><img src={select} /></div>
+      <div className='document__footer__add'>
+        <div className='document__footer__add__button'>Add</div>
       </div>
+    </div>
   )
 }
 export default Footer
